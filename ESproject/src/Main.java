3519ef3d2c78f84830eb4645d06f7aed7630348f
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2024-07-23 16:35:01 +0200

    es-2 commit 2

--- ESproject/src/Main.java
+++ ESproject/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("!");
+		System.out.println("!!");  // es2
+		System.out.println("!!!"); // es2 commit2
 	}
 
 }

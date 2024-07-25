3f430e73458ef98c3acc6c2ef570a215d2675345
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2024-07-25 14:21:27 +0200

    es-7 completo

--- ESproject/src/Main.java
+++ ESproject/src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
 
+	static void met (String value) {
+		System.out.println("qqqq");
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("!");
@@ -8,4 +13,7 @@ public class Main {
 		System.out.println("!!!"); // es2 commit2
 	}
 
+	static void met2 (String value) {
+		System.out.println("qqqq");
+	}
 }
